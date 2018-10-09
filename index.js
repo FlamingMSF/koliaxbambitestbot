6ec07def6afeb898a226950aecb6ea8712c67505
FlamingMSF <43052300+FlamingMSF@users.noreply.github.com>
2018-10-09 15:28:12 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const Commando = require('discord.js-commando');
+const bot = new Commando.Client();
+
+bot.registry.registerGroup('simple', 'simple');
+bot.registry.registerDefaults();
+bot.registry.registerCommandsIn(__dirname + '/commands')
+
+bot.on('message', function(message){
+    if(message.content == 'Ping')
+    {
+        message.channel.sendMessage('Pong');
+    }
+});
+
+bot.on('ready', function(){
+    console.log('Ready')
+});
+
+client.login(process.env.BOT_TOKEN);
