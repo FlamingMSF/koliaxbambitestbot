8f3989e52ec1e18587b1c21269fea345bb6c133e
FlamingMSF <43052300+FlamingMSF@users.noreply.github.com>
2018-10-10 20:18:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const Commando = require('discord.js-commando');
 const bot = new Commando.Client();
 
+bot.registry.registerGroup('simple', 'Simple');
+bot.registry.registerGroup('music', 'Music');
+bot.registry.registerDefaults();
+bot.registry.registerCommandsIn(__dirname + '/commands')
+
 bot.on('message', function(message){
     if(message.content == 'Ping')
     {
