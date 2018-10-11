952d045a17ae450bd51ce91694c9d045682e1f7d
FlamingMSF <43052300+FlamingMSF@users.noreply.github.com>
2018-10-10 20:09:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const Commando = require('discord.js-commando');
-const bot = new Commando.Client();
+const client = new Commando.Client();
 
-bot.on('message', function(message){
+client.on('message', function(message){
     if(message.content == 'Ping')
     {
         message.channel.send('Pong');
     }
 });
 
-bot.on('ready', function(){
+client.on('ready', function(){
     console.log('Ready')
 });
 
-bot.login(process.env.TOKEN)
+client.login(process.env.TOKEN)
